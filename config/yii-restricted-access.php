753355b46b76208964511aa7c494815fc44157bd
Fredrik Wollsén <fredrik@neam.se>
2014-09-26 15:29:14 +0200

    Imported more logic and added relevant imports to config include

--- config/yii-restricted-access.php
+++ config/yii-restricted-access.php
@@ -1,5 +1,7 @@
 <?php
 
+$config['import'][] = 'vendor.neam.yii-restricted-access.behaviors.*';
+$config['import'][] = 'vendor.neam.yii-restricted-access.helpers.*';
 $config['modules']['restrictedAccess'] = array(
     'class' => 'vendor.neam.yii-restricted-access.modules.restrictedAccess.RestrictedAccessModule',
 );

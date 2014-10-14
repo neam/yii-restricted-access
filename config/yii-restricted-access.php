1f886a3bb3a52ca43b044542212ecc9930b4b111
Fredrik Wollsén <fredrik@neam.se>
2014-10-15 01:14:33 +0200

    Added a missing import in config include

--- config/yii-restricted-access.php
+++ config/yii-restricted-access.php
@@ -2,6 +2,7 @@
 
 $config['import'][] = 'vendor.neam.yii-restricted-access.behaviors.*';
 $config['import'][] = 'vendor.neam.yii-restricted-access.helpers.*';
+$config['import'][] = 'vendor.neam.yii-restricted-access.modules.restrictedAccess.widgets.*';
 $config['modules']['restrictedAccess'] = array(
     'class' => 'vendor.neam.yii-restricted-access.modules.restrictedAccess.RestrictedAccessModule',
 );
